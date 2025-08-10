ae9f9c0c45070ad87a6b45278a9ba476b94f48bc
ksuds08 <65447814+ksuds08@users.noreply.github.com>
2025-08-10 12:52:59 -0400

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,1 +1,7 @@
-
+export default {
+  root: true,
+  parserOptions: { ecmaVersion: "latest", sourceType: "module", project: false },
+  env: { es2022: true, worker: true },
+  extends: ["eslint:recommended"],
+  rules: { "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }] }
+};
