3d027b7424c20156f73b4d3b43f84d7618bdd687
ksuds08 <65447814+ksuds08@users.noreply.github.com>
2025-08-10 12:57:42 -0400

    Update log.ts

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,1 +1,15 @@
-log.ts
+export function json(obj: unknown, status = 200): Response {
+  return new Response(JSON.stringify(obj), { status, headers: { "content-type": "application/json" } });
+}
+
+export function withReqId<T extends (req: Request, ...args: any[]) => Promise<Response>>(
+  handler: T
+) {
+  return async (req: Request, ...rest: any[]) => {
+    const reqId = crypto.randomUUID();
+    const res = await handler(req, ...rest);
+    const headers = new Headers(res.headers);
+    headers.set("x-request-id", reqId);
+    return new Response(await res.text(), { status: res.status, headers });
+  };
+}
